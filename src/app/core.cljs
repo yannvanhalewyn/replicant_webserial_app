4799eb30ab7fc0011ffbfbc2a7e8739ec5a24f0d
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2025-10-29 20:17:29 +0100

    (fix): render calls

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,5 +1,5 @@
 (ns app.core
-  (:require [replicant.dom :as d]
+  (:require [replicant.dom :as r]
             [app.webserial :as serial]))
 
 (defonce state (atom {:message "Hello, Replicant!"
@@ -7,37 +7,36 @@
                       :serial-status "Not connected"}))
 
 (defn view [state]
-  (d/html
+  [:div
+   [:h1 (:message state)]
+   [:p "This is a Replicant app with WebSerial API support."]
+
+   [:div.status
+    [:strong "Serial Status: "] (:serial-status state)]
+
    [:div
-    [:h1 (:message state)]
-    [:p "This is a Replicant app with WebSerial API support."]
+    [:button
+     {:on-click #(serial/connect! state)}
+     "Connect to Serial Device"]
 
-    [:div.status
-     [:strong "Serial Status: "] (:serial-status state)]
+    [:button
+     {:disabled (not (:serial-connected state))
+      :on-click #(serial/disconnect! state)}
+     "Disconnect"]
 
-    [:div
-     [:button
-      {:on-click #(serial/connect! state)}
-      "Connect to Serial Device"]
-
-     [:button
-      {:disabled (not (:serial-connected state))
-       :on-click #(serial/disconnect! state)}
-      "Disconnect"]
-
-     [:button
-      {:disabled (not (:serial-connected state))
-       :on-click #(serial/send-data! "Hello from browser!\n")}
-      "Send Test Message"]]]))
+    [:button
+     {:disabled (not (:serial-connected state))
+      :on-click #(serial/send-data! "Hello from browser!\n")}
+     "Send Test Message"]]])
 
 (defn render! []
-  (d/render (js/document.getElementById "app")
-            (view @state)))
+  (r/render (js/document.getElementById "app")
+    (view @state)))
 
 (defn init! []
   (println "Initializing app...")
   (add-watch state ::render
-             (fn [_ _ _ _] (render!)))
+    (fn [_ _ _ _] (render!)))
   (render!))
 
 (defn reload! []
