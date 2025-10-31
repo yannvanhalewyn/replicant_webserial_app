4b91c96279029305e731f9ce5fe0f366942c01b1
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2025-10-31 21:01:59 +0100

    (dev): improve colorscheme of cljs devtools

--- env/dev/dev/preload.cljs
+++ env/dev/dev/preload.cljs
@@ -1,4 +1,8 @@
 (ns dev.preload
   (:require [devtools.core :as devtools]))
 
+(let [{:keys [cljs-land-style]} (devtools/get-prefs)]
+  (devtools/set-pref! :cljs-land-style
+    (str "filter:invert(1);" cljs-land-style)))
+
 (devtools/install!)
